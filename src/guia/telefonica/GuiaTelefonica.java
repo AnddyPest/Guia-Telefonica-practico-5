40717227bca1dc4945ec6d195ca3585b631fea4b
AnddyPest <mauripestchanker@gmail.com>
2024-09-02 16:29:09 -0300

    Initial commit

---
+++ src/guia/telefonica/GuiaTelefonica.java
@@ -0,0 +1,49 @@
+
+
+package guia.telefonica;
+
+import java.util.Objects;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author mauri
+ */
+public class GuiaTelefonica {
+
+    public static TreeMap<Long, Persona> cliente = new TreeMap();
+
+    public TreeMap<Long, Persona> getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(TreeMap<Long, Persona> cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.cliente);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GuiaTelefonica other = (GuiaTelefonica) obj;
+        return Objects.equals(this.cliente, other.cliente);
+    }
+
+   
+    
+    
+}
---
+++ src/guia/telefonica/Persona.java
@@ -0,0 +1,134 @@
+package guia.telefonica;
+
+import java.util.Objects;
+import java.util.TreeSet;
+
+
+public class Persona implements Comparable<Persona>{
+
+    int dni;
+    String nombre;
+    String apellido;
+    String ciudad;
+    String direccion;
+    TreeSet<Persona> cliente;    
+
+    public Persona(int dni, String nombre, String apellido, String ciudad, String direccion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.cliente = new TreeSet();
+        
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public TreeSet<Persona> getPersona() {
+        return cliente;
+    }
+
+    public void setPersona(TreeSet<Persona> persona) {
+        this.cliente = persona;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.dni;
+        hash = 83 * hash + Objects.hashCode(this.nombre);
+        hash = 83 * hash + Objects.hashCode(this.apellido);
+        hash = 83 * hash + Objects.hashCode(this.ciudad);
+        hash = 83 * hash + Objects.hashCode(this.direccion);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (this.dni != other.dni) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellido, other.apellido)) {
+            return false;
+        }
+        if (!Objects.equals(this.ciudad, other.ciudad)) {
+            return false;
+        }
+        return Objects.equals(this.direccion, other.direccion);
+    }
+
+    @Override
+    public String toString() {
+        return "DNI: "+dni+" - "+"Apellido: "+apellido+" - "+"Nombre: "+nombre+" - "+ciudad+" - "+"Direccion: "+direccion;
+    }
+   ;
+
+    @Override
+    public int compareTo(Persona o) {
+        return this.apellido.compareTo(o.getApellido());
+        
+        
+    
+    }
+
+    
+    
+    
+    
+    
+    
+}
