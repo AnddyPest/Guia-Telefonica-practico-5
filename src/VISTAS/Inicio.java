ab3498b6a1de6e6fdad53604a907373a7b331ead
AnddyPest <mauripestchanker@gmail.com>
2024-09-02 21:13:36 -0300

    comentado el error

--- src/VISTAS/Inicio.java
+++ src/VISTAS/Inicio.java
@@ -322,7 +322,7 @@ public class Inicio extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        srcScrn.setVisible(true);
+        //srcScrn.setVisible(true); cambiar el nombre del frame
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void btnNewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewActionPerformed
